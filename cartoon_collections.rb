3a7044993b48f55b72ed83f5740a6ee3eefe5f01
Jorge Venegas <brangelina01@gmail.com>
2020-07-15 22:13:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,10 @@ end
 
 def find_the_cheese(array)
 array_x = ["cheddar", "gouda", "camembert"]
-if array_x.include("cheddar")
-  true 
-else
-end 
+  if array_x.include("cheddar")
+    true 
+  end 
+
 
 
 
